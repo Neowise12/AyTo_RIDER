f0eeb732c076094d9391b39320ca6c91c10c4d76
Debarun Acharya <debarunacharya2002@gmail.com>
2022-06-30 16:13:25 +0530

    update on datafetch-2

--- lib/backendwork/DetailsFetch.dart
+++ lib/backendwork/DetailsFetch.dart
@@ -1,5 +1,48 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:http/http.dart' as http;
+Future<String> getJsonFromFirebaseRestAPI() async {
+  String url = "https://ayto-rkmgec-rev2-default-rtdb.firebaseio.com/ONLINE.json";
+  http.Response response = await http.get(Uri.parse(url));
+  return response.body;
+}
 
+class SalesData {
+
+  //SalesData(this.month, this.sales);
+
+  var UID;
+  var state;
+  SalesData(this.UID, this.state);
+  factory SalesData.fromJson(Map<String, dynamic> parsedJson) {
+    return SalesData(
+      parsedJson['UID'].toString(),
+      parsedJson['state'].toString(),
+    );
+  }
+  List<SalesData> chartData = [];
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 class Need {
   var id;
   var lat;
@@ -13,11 +56,13 @@ class Need {
     required this.state,
   });
 
-  /*Need.fromSnapshot(DataSnapshot snapshot, this.lat, this.long, this.state) :
+  */
+/*Need.fromSnapshot(DataSnapshot snapshot, this.lat, this.long, this.state) :
         id = snapshot.key,
         lat = snapshot.value,
         long = snapshot.value,
-        state = snapshot.value;*/
+        state = snapshot.value;*//*
+
 
   Future<DatabaseEvent> getItems() {
     return FirebaseDatabase().reference().child("items").once();
@@ -36,4 +81,4 @@ class Need {
     // TODO: implement print
     throw UnimplementedError();
   }
-}
+}*/
