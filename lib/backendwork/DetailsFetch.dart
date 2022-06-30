cb85277eeecde9f10ccdd539bbf2cf937eaca852
Debarun Acharya <debarunacharya2002@gmail.com>
2022-06-30 14:54:32 +0530

    Datafetch Trial -Debarun Acharya

--- lib/backendwork/DetailsFetch.dart
+++ lib/backendwork/DetailsFetch.dart
@@ -1,65 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:after_layout/after_layout.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:rider/LoginPages/signUp_page.dart';
-import 'package:rider/mainPage/mainpage.dart';
-import 'package:rider/LoginPages/login_Page.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-class LoginState extends StatefulWidget {
-  @override
-  HomeScreenState createState() => new HomeScreenState();
+class Need {
+  final String id;
+  final String lat;
+  final String long;
+  final String state;
+
+  Need({
+    required this.id,
+    required this.lat,
+    required this.long,
+    required this.state,
+  });
+
+/*  Need.fromSnapshot(DataSnapshot snapshot, this.lat, this.long, this.state) :
+        id = snapshot.key,
+        imageUrl = snapshot.value["lat"],
+        caption = snapshot.value["long"],
+        title = snapshot.value["state"];*/
+
+  toJson() {
+    return {
+      "lat": lat,
+      "long": long,
+      "state": state,
+    };
+  }
 }
-
-class HomeScreenState extends State<LoginState> with AfterLayoutMixin<LoginState> {
-
-  FirebaseAuth auth = FirebaseAuth.instance;
-
-  String srm="";
-  final _database=FirebaseDatabase.instance.ref();
-  int c=0;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    activeListeners();
-  }
-  void activeListeners(){
-    final User? user=auth.currentUser;
-    final id=user?.uid ;
-    _database.child('ONLINE/').onValue.listen((event) {
-      final Object? description=event.snapshot.value ?? false;
-      setState(() {
-        print('DescriptionLS=$description');
-        srm='$description';
-        print('SRMx1=$srm');
-
-        if(srm.compareTo('false')==0) {
-          c++;
-        }
-
-
-
-
-      });
-    });
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return new Scaffold(body: new Container(color: Colors.white));
-  }
-
-  @override
-  void afterFirstLayout(BuildContext context) {
-    // Calling the same function "after layout" to resolve the issue.
-
-    activeListeners();
-  }
-
-
-
-  }
